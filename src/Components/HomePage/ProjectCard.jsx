015477916e73073b7f08ce0e3259a2f5474be990
Rushikesh7997 <pardhikar26@gmail.com>
2022-10-01 02:08:47 +0530

    day 4 update

--- src/Components/HomePage/ProjectCard.jsx
+++ src/Components/HomePage/ProjectCard.jsx
@@ -1,21 +1,110 @@
-import React from 'react'
+import React, { useState } from 'react';
+import {
+  Center,
+  Box,
+  Flex,
+  InputGroup,
+  InputLeftElement,
+  Wrap,
+  SimpleGrid,
+  Button,
+  Stack,
+  Input,
+  Image,
+  Text,
+  Grid,
+  Progress,
+  Select,
+  WrapItem,
+  GridItem,
+} from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
 
-export const ProjectCard = ({id,img,title,fund,description,category,percentage,daysLeft}) => {
+export const ProjectCard = ({
+  id,
+  img,
+  title,
+  fund,
+  description,
+  category,
+  percentage,
+  daysLeft,
+}) => {
+
+ 
+
   return (
-    <div>
-        <div className='single_new_product'>
-        {/* <Link to={`/${id}`}> */}
-            <div className='card'>
-                <img src={img} alt={title} />
-            </div> 
-            <p className='single_product_title'>{title}</p>
-            <p>{description}</p>
-            <p>{category}</p>
-            <p className='single_product_price'>${fund}USD raised</p>
-            <p>{percentage}%</p>
-            <p>{daysLeft} days left</p>
-            {/* </Link> */}
-        </div>
-    </div>
-  )
-}
+   
+//       <Grid
+//   h='500px'
+//   templateRows='repeat(2, 1fr)'
+//   templateColumns='repeat(5, 1fr)'
+// >
+//                   <GridItem key={id} border="1px solid #e1e1e1" textAlign="left">
+//                     {/* <Link to={`/SingleProductPage/${id}`}> */}
+//                       <Image src={img} w="100%" />
+//                       <Box padding={'20px'}>
+//                         <Box mb={'10px'}>
+//                           <Flex
+//                             justifyContent="space-between"
+//                             gap="10px"
+//                             alignItems={'center'}
+//                           >
+//                             <Text fontSize="sm" color="teal">
+//                               {category}
+//                             </Text>
+//                             {/* <AiOutlineHeart /> */}
+//                           </Flex>
+//                         </Box>
+//                         <hr />
+//                         <Text fontSize="lg" fontWeight="550">
+//                           {title}
+//                         </Text>
+//                         <p
+//                           fontWeight="500"
+//                           style={{
+//                             fontSize: '13.5px',
+//                             color: 'rgb(122,121,121)',
+//                             marginBottom: '20px',
+//                           }}
+//                         >
+//                           {description}
+//                         </p>
+
+//                         <h3>AUDIO</h3>
+//                         <Box>
+//                           <Flex
+//                             justifyContent={'space-between'}
+//                             alignItems={'center'}
+//                           >
+//                             <Box>
+//                               <h2>{fund}</h2>
+//                               <p>raised</p>
+//                             </Box>
+//                             <Box>
+//                               <p>{percentage}%</p>
+//                             </Box>
+//                           </Flex>
+//                         </Box>
+//                         <Progress
+//                           borderRadius={'10px'}
+//                           colorScheme="green"
+//                           size="sm"
+//                           value={percentage}
+//                         />
+//                         <Box>
+//                           {daysLeft === 'Now Funding through InDemand'}
+//                           <p>
+//                             {daysLeft === 'Now Funding through InDemand'
+//                               ? daysLeft
+//                               : daysLeft + ' days left'}
+//                           </p>
+//                         </Box>
+//                       </Box>
+//                     {/* </Link> */}
+//                   </GridItem>
+//                   </Grid>
+      <div></div>
+
+  );
+};
--- src/Routes/CheckoutPage.jsx
+++ src/Routes/CheckoutPage.jsx
@@ -1,8 +1,106 @@
+import { Radio } from '@chakra-ui/react'
 import React from 'react'
+import styles from "../Styles/CheckoutPage.module.css"
+
 
 const CheckoutPage = () => {
   return (
-    <div>CheckoutPage</div>
+    <div>
+        <div className={styles.mainContainerR}>
+          <div className={styles.mainContinerR1}>
+              <div className={styles.youContribution}>You're contributing to</div>
+              <div className={styles.deagonfly}>Deagonfly - Ride the World's First Hyperscooter</div>
+              <div className={styles.payflow_backbutton}>Back</div>
+              <div className={styles.payoff_logindata}>
+                <div >
+                  <img src="https://g2.iggcdn.com/assets/individuals/missing/thumbnail-deaf450c2d4183b9309b493f6a7b20d62f8d31617ec828d060df465abe92ef2a.png" alt="" style={{ "border-radius":"50px"}} />
+                </div>
+                <div>
+                  <div className={styles.payoff_logindata1} > Name </div>
+                  <div className={styles.payoff_logindata1} > Email </div>
+                </div>
+              </div>
+              <div className={styles.payflow_shipping}>Shipping Address</div>
+              <div className={styles.payflow_newAdress}> <Radio></Radio> New Address</div>
+              <div className={styles.payflow_box1}>
+                <div className={styles.payflow_fullname}>Full Name*</div>
+                <div><input type="text" className={styles.payflow_inputbox} /></div>
+                <div className={styles.payflow_fullname}>Country*</div>
+                <div><input type="text" className={styles.payflow_inputbox} /></div>
+                <div className={styles.payflow_fullname}>Street Address*</div>
+                <div><input type="text" className={styles.payflow_inputbox} /></div>
+                <div className={styles.payflow_fullname}>City</div>
+                <div><input type="text" className={styles.payflow_inputbox} /></div>
+                <div className={styles.payflow_fullname}>Postal Code*</div>
+                <div><input type="text" className={styles.payflow_inputbox} /></div>
+                <div className={styles.payflow_fullname}>Phone Number*</div>
+                <div><input type="text" className={styles.payflow_inputbox} /></div>
+              </div>
+
+              <br />
+
+              <div className={styles.payflow_shipping}>Secure Payment </div>
+              <div className={styles.payflow_box1}>
+                <div className={styles.payflow_fullname}>Card number</div>
+                <div><input type="text" className={styles.payflow_inputbox} placeholder="1234 1234 1234 1234" /></div>
+                <div className={styles.payflow_fullname}>Expiration</div>
+                <div><input type="text" className={styles.payflow_inputbox} placeholder="MM / YY" /></div>
+                <div className={styles.payflow_fullname}>CVC</div>
+                <div><input type="text" className={styles.payflow_inputbox} placeholder="CVC Number" /></div>
+                <div className={styles.payflow_fullname}>Country</div>
+                <div><input type="text" className={styles.payflow_inputbox} placeholder="INDIA" /></div>
+              </div>
+
+              <div className={styles.payflow_fullname}>Add a tip Indiegogo</div>
+              <div>By supporting Indiegogo, you are hepling us bring the power of community to <br /> important causes around the world. </div>
+              <div className={styles.payflow_newAdress}> <Radio></Radio> 10%($175 GBP)</div>
+              <div className={styles.payflow_newAdress}> <Radio></Radio> 15%($263 GBP)</div>
+              <div className={styles.payflow_newAdress}> <Radio></Radio> 20%($350 GBP)</div>
+              <div className={styles.payflow_newAdress}> <Radio></Radio> Other Amount</div>
+          </div>
+          <div>
+            <div className={styles.seconContainerPayoff}>
+              <div className={styles.payflow_fullname}>Contribution summary</div>
+              <div className={styles.payoffContinnerflex}>
+                <div className={styles.payoff_images}>
+                  <img src="https://c3.iggcdn.com/indiegogo-media-prod-cld/image/upload/c_fit,w_72,g_center,q_auto:eco,dpr_1.0,f_auto,h_48/ogih9n8n0xrm8mpkqvku" alt="" />
+                </div>
+                <div className={styles.payflow_box1containe}>1X Dragonfly DF</div>
+                <div className={styles.payflow_fullname}>$1,750 GBP</div>
+              </div>
+              <hr />
+              <div className={styles.payoffContinnerflex}>
+                <div className={styles.payflow_shippingtiles}>SubTotal</div>
+                <div className={styles.payflow_fullname}>$1,750 GBP</div>
+              </div>
+
+              <div className={styles.payoffContinnerflex}>
+                <div className={styles.payflow_shippingtiles}>Shipping</div>
+                <div className={styles.payflow_fullname}>$-GBP</div>
+              </div>
+
+              <div>
+                <input type="text" className={styles.payflow_inputbox} placeholder="Type a shipping destination"/>
+              </div>
+
+              <div className={styles.payoffContinnerflex}>
+                <div className={styles.payflow_shippingtiles}>Indiegogo Tip</div>
+                <div className={styles.payflow_fullname}>$ 236 GBP</div>
+              </div>
+
+              <div className={styles.payoffContinnerflex}>
+                <div className={styles.payoff_Total}>TOTAL</div>
+                <div className={styles.payoff_Total}>$ {2013} GBP</div>
+              </div>
+
+              <div>
+                <button className={styles.payoff_submitButton}>Submit Payment</button>
+              </div>
+
+            </div>
+          </div>
+        </div>
+    </div>
   )
 }
 
