c381211be81b6c88739ab72bea336e535f063162
Rushikesh7997 <pardhikar26@gmail.com>
2022-09-30 10:51:36 +0530

    day-3 homepage

--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { FindITFirst } from '../Components/HomePage/FindITFirst'
+import { ImageTiles } from '../Components/HomePage/ImageTiles'
 import { PopularProject } from '../Components/HomePage/PopularProject'
 import { Slider } from '../Components/HomePage/Slider'
 
@@ -9,6 +10,7 @@ const Home = () => {
       <Slider/>
       <FindITFirst/>
       <PopularProject/>
+      <ImageTiles/>
     </div>
   )
 }
