d26e69ffc57a209f2e8d98797d80b009e01a56e0
anjanak05 <anjna4523@gmail.com>
2022-10-01 17:07:14 +0530

    login and signup

--- src/Redux/authReducer/action.js
+++ src/Redux/authReducer/action.js
@@ -3,15 +3,34 @@ import axios from 'axios'
 
 
 const login = (payload)=> dispatch=>{
-  console.log(payload)
+// let username = payload.username  
+// const navigate = useNavigate()
+
+
+  console.log(payload.username)
     dispatch({type:types.USER_LOGIN_REQUEST})
-    return axios.post("https://masai-api-mocker.herokuapp.com/auth/login", payload)
+    return axios.post("https://masai-api-mocker.herokuapp.com/auth/login", payload.loginData)
     .then((r)=>{
       console.log(r)
-     return dispatch({type:types.USER_LOGIN_SUCCESS, payload:r.data.token})
+   return dispatch({type:types.USER_LOGIN_SUCCESS, payload:r.data.token})
+    
     })
     .catch((err)=>{
       dispatch({type:types.USER_LOGIN_FAILURE,payload:err})
     })
   }
   export {login}
+
+  const register = (payload)=> dispatch=>{
+    console.log(payload)
+      dispatch({type:types.USER_LOGIN_REQUEST})
+      return axios.post("https://masai-api-mocker.herokuapp.com/auth/register", payload)
+      .then((r)=>{
+        console.log(r)
+       return dispatch({type:types.USER_SIGNUP_SUCCESS, payload:r.data})
+      })
+      .catch((err)=>{
+        dispatch({type:types.USER_LOGIN_FAILURE,payload:err})
+      })
+    }
+export{register}
